f506a2976fb56ddd6cfa57971a1c367242d86f25
yoavweber <yoav.weber@code.berlin>
2021-04-26 11:06:10 +0200

    adding object extraction to Jorgen algorithem

--- main.m
+++ main.m
@@ -18,8 +18,14 @@ for i=1:size(names_cell,2)
     % get the corder box and compare it with the answer borderbox
     algoResult = jAlgoritmeColor(fishimage);
     answerCords = getAnswerCords(filename);
+
+    res = []
+    for k=1:size(algoResult,2)
+       res(end + 1) = compareBoxes(algoResult(k,:), answerCords, fishimage)
+    end
+
     result = compareBoxes(algoResult, answerCords, fishimage);
-    allResults(end+1) = result;
+    allResults(end+1) = max(res);
 end
 
 allResults
